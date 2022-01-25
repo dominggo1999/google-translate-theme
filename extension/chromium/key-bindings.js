e285f58806c245139e53ff286278991141aa210d
dominggo1999 <arnolddominggo19@gmail.com>
2022-01-25 14:54:40 +0700

    key shortcuts : select language

--- extension/chromium/key-bindings.js
+++ extension/chromium/key-bindings.js
@@ -6,6 +6,10 @@ window.addEventListener('load', () => {
 
     const ttsTarget = document.querySelector('.BdDRKe > .aJIq1d.DSmisd div:nth-child(3) > div:nth-child(1) > span > button');
 
+    const selectSource = document.querySelector('#yDmH0d > c-wiz > div > div.WFnNle > c-wiz > div.OlSOob > c-wiz > div.ccvoYb > div.aCQag > c-wiz > div.zXU7Rb > c-wiz > div:nth-child(2) > button > div.VfPpkd-Bz112c-RLmnJb');
+
+    const selectTarget = document.querySelector('#yDmH0d > c-wiz > div > div.WFnNle > c-wiz > div.OlSOob > c-wiz > div.ccvoYb > div.aCQag > c-wiz > div.zXU7Rb > c-wiz > div:nth-child(5) > button > div.VfPpkd-Bz112c-RLmnJb');
+
     const key = e.key;
 
     if(key === 'q' && e.ctrlKey) {
@@ -24,6 +28,14 @@ window.addEventListener('load', () => {
       setTimeout(() => {
         inputArea.focus();
       }, 600);
+    }
+
+    if(key === ';' && e.ctrlKey) {
+      selectSource && selectSource.click();
+    }
+
+    if(key === "'" && e.ctrlKey) {
+      selectTarget && selectTarget.click();
     }
   });
 });
