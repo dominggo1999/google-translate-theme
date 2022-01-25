a8df3ba9acbaab84eb7e297cc37b0225f8ea4b85
dominggo1999 <arnolddominggo19@gmail.com>
2022-01-25 14:37:47 +0700

    mozilla : add basic key shortcuts

---
+++ extension/mozilla/key-bindings.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', () => {
+  const inputArea = document.querySelector('textarea');
+
+  window.addEventListener('keydown', (e) => {
+    const ttsSource = document.querySelector('div.r375lc > div > div:nth-child(3) > div:nth-child(1) > span > button');
+
+    const ttsTarget = document.querySelector('.BdDRKe > .aJIq1d.DSmisd div:nth-child(3) > div:nth-child(1) > span > button');
+
+    const key = e.key;
+
+    if(key === 'q' && e.ctrlKey) {
+      inputArea && inputArea.focus();
+    }
+
+    if(key === ',' && e.ctrlKey) {
+      ttsSource && ttsSource.click();
+      setTimeout(() => {
+        inputArea.focus();
+      }, 500);
+    }
+
+    if(key === '.' && e.ctrlKey) {
+      ttsTarget && ttsTarget.click();
+      setTimeout(() => {
+        inputArea.focus();
+      }, 600);
+    }
+  });
+});
